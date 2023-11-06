3af86b28c9782d2dd0761a03d72b5a18f85b3e3e
tomoyo519 <tomoyo519@naver.com>
2023-11-06 23:23:13 +0900

    cors

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const db = JSON.parse(fs.readFileSync(path.join(__dirname, "db.json")));
 const middlewares = jsonServer.defaults();
 const auth = require("json-server-auth");
 const router = jsonServer.router(db);
+const cors = require("cors");
 server.db = router.db;
 // Set default middlewares (logger, static, cors and no-cache)
 server.use(middlewares);
@@ -28,6 +29,15 @@ server.use((req, res, next) => {
 // Use default router
 server.use(auth);
 server.use(router);
+server.use(
+  cors({
+    origin: true,
+    credentials: true,
+    preflightContinue: false,
+    methods: "GET, HEAD,PUT,POST,DELETE, PATCH",
+  })
+);
+server.options("*", cors());
 server.listen(3001, () => {
   console.log("JSON Server is running");
 });
